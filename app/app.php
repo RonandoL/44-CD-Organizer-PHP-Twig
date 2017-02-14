f0b566ac632039198b8a141463b43843c96c87a2
Ronando Long <ronando.long@gmail.com>
2017-02-13 17:33:09 -0800

    add ability for user to enter CD title and display titles in browser, and delete all CDs

--- app/app.php
+++ app/app.php
@@ -3,7 +3,7 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/cd.php";
 
-    session_start();                          // For global variable, saving in browser cache
+    session_start();
     if (empty($_SESSION['list_of_cds'])) {
         $_SESSION['list_of_cds'] = array();
     }
@@ -16,13 +16,23 @@
   // End Red Tape
 
   // 1. Route for home page
-    $app
+    $app->get('/', function() use ($app) {
+        return $app['twig']->render('cds.html.twig', array('cds' => $_SESSION['list_of_cds']));
+    });
 
-  // 2. Route for sending instantiated new object (new task) to /tasks URL
-    $app
+  // 2. POST INSTANTIATE Route for sending new object (new task) to /new-cd URL
+    $app->post('/new-cd', function() use ($app) {
+        $cd = new Cd($_POST['title']);
+        $cd->save();
+
+        return $app['twig']->render('cds.html.twig', array('cds' => $_SESSION['list_of_cds']));
+    });
 
   // 3. Route for deleting all tasks
-    $app
+    $app->post('/', function() use ($app) {
+        Cd::deleteAll();
+        return $app['twig']->render('cds.html.twig');
+    });
 
     return $app;
 
--- src/cd.php
+++ src/cd.php
@@ -1,6 +1,33 @@
 <?php
-    class cd
+    class Cd
     {
-        
+        private $title;
+
+        function __construct($title)
+        {
+            $this->title = $title;
+        }
+
+        function getTitle()
+        {
+            return $this->title;
+        }
+
+        function save()
+        {
+            array_push($_SESSION['list_of_cds'], $this);
+        }
+
+        static function getAll()
+        {
+            return $_SESSION['list_of_cds'];
+        }
+
+        static function deleteAll()
+        {
+            $_SESSION['list_of_cds'] = array();
+        }
+
+
     }
 ?>
